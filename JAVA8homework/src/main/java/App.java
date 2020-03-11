f25837f2da1b16c61a9330b0b4c3b5fba1560776
lpdft <772346730@qq.com>
2020-03-12 01:55:52 +0800

    [ruyue] feat: print MasterNumber equals 1,2 and 3 user informations

--- JAVA8homework/src/main/java/App.java
+++ JAVA8homework/src/main/java/App.java
@@ -1,10 +1,25 @@
+
+import entity.MasterNumber;
+import entity.Person;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Stream;
+
 public class App {
 
   public static void main(String[] args) {
     PersonService personService = new PersonService();
     //TODO: print Person data that masterNumber is 1 and 2
     // print Person data that masterNumber is 3
+    Stream<Person> personByMasterNumbers1andNumber2 = personService.getPersonByMasterNumbers(Arrays.asList(
+            new MasterNumber("1"), new MasterNumber("2")));
+    System.out.println("编号为1和2的用户详细信息：");
+    personByMasterNumbers1andNumber2.forEach(System.out::println);
 
+    Stream<Person> personByMasterNumber3 = personService.getPersonByMasterNumbers(Collections.singletonList(new MasterNumber("3")));
+    System.out.println("编号为3的用户详细信息：");
+    personByMasterNumber3.forEach(System.out::println);
   }
 
 }
